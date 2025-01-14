9e0d86e5075cd3fd21021d2ea35035b25f4c803d
diegozamora <diarzaza@gmail.com>
2025-01-14 08:42:59 -0500

    cambios encomparar fechas

--- CompararFechas.py
+++ CompararFechas.py
@@ -1,81 +1,147 @@
 import tkinter as tk
+from tkinter import simpledialog
 
+import messagebox
+import mysql.connector
+from time import strftime
+
+def conectar_db():
+    conn = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="",
+        database="Tomas_pizza",
+        port="3306"
+    )
+    return conn
+
+def obtener_gastos(fecha):
+    conn = conectar_db()
+    cursor = conn.cursor()
+    query= f"SELECT SUM(valor_gasto) FROM gastos WHERE fecha LIKE '{fecha}%'"
+    cursor.execute(query)
+    resultado = cursor.fetchone()
+    conn.close()
+    return resultado[0] if resultado[0] else 0
+
+def obtener_cierre(fecha):
+    conn = conectar_db()
+    cursor = conn.cursor()
+    query = f"SELECT Total_general FROM cierre WHERE Ventas_fecha = '{fecha}'"
+    cursor.execute(query)
+    resultado = cursor.fetchone()
+    conn.close()
+    return resultado[0] if resultado else 0
 
 def abrir_comparar_fechas(ventana_Bienvenida):
-    Ventana = tk.Toplevel(ventana_Bienvenida)
-    Ventana.title("ventana Tomas")
-    Ventana.geometry(f"510x600+400+50")
-    Ventana.configure(bg="#FDF5E6")
-    Ventana.resizable(False, False)
+        Ventana = tk.Toplevel(ventana_Bienvenida)
+        Ventana.title("ventana Tomas")
+        Ventana.geometry(f"510x600+400+50")
+        Ventana.configure(bg="#FDF5E6")
+        Ventana.resizable(False, False)
 
-    def volver():
-        Ventana.destroy()
-        ventana_Bienvenida.deiconify()
+        # Fecha y hora
 
-    # Fecha y hora
-    from time import strftime
-    def actualizar_reloj():
-        tiempo_actual = strftime('%H:%M:%S %p')
-        fecha_actual = strftime('%Y-%m-%d')
-        etiqueta_hora.config(text=tiempo_actual)
-        etiqueta_fecha.config(text=fecha_actual)
-        Ventana.after(100, actualizar_reloj)
+        from time import strftime
+        def actualizar_reloj():
+            tiempo_actual = strftime('%H:%M:%S %p')
+            fecha_actual = strftime('%Y-%m-%d')
+            etiqueta_hora.config(text=tiempo_actual)
+            etiqueta_fecha.config(text=fecha_actual)
+            Ventana.after(100, actualizar_reloj)
 
-    marco_derecho = tk.Frame(Ventana, background='#FDF5E6')
-    marco_derecho.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        def volver():
+            Ventana.destroy()
+            ventana_Bienvenida.deiconify()
 
-    etiqueta_hora = tk.Label(Ventana, font=('cabril', 10, 'bold'), background='#FDF5E6', foreground='BLACK')
-    etiqueta_hora.pack(anchor='center')
 
-    etiqueta_fecha = tk.Label(Ventana, font=('cabril', 10, 'bold'), background='#FDF5E6', foreground='BLACK')
-    etiqueta_fecha.pack(anchor='center')
-    actualizar_reloj()
+        marco_derecho = tk.Frame(Ventana, background='#FDF5E6')
+        marco_derecho.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-    Ventana.title()
-    etiqueta_total = tk.Label(Ventana, text="COMPARAR FECHAS")
-    etiqueta_total.config(fg="red", bg="#FDF5E6", font=("Open Sans", 20, "bold"))
-    etiqueta_total.place(x=110, y=30)
+        etiqueta_hora = tk.Label(Ventana, font=('cabril', 10, 'bold'), background='#FDF5E6', foreground='BLACK')
+        etiqueta_hora.pack(anchor='center')
 
-    boton_editar_pedido = tk.Button(Ventana, text="FECHA A")
-    boton_editar_pedido.configure(fg="black", bg="#a6a6a6", font=("Open Sans", 10), width=13)
-    boton_editar_pedido.place(x=50, y=100)
+        etiqueta_fecha = tk.Label(Ventana, font=('cabril', 10, 'bold'), background='#FDF5E6', foreground='BLACK')
+        etiqueta_fecha.pack(anchor='center')
 
-    boton_editar_pedido = tk.Button(Ventana, text="FECHA B ")
-    boton_editar_pedido.configure(fg="black", bg="#a6a6a6", font=("Arial", 10), width=13)
-    boton_editar_pedido.place(x=320, y=100)
+        actualizar_reloj()
 
-    etiqueta_pedido = tk.Label(Ventana, text="FECHA DD/MM/AA: ")
-    etiqueta_pedido.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
-    etiqueta_pedido.place(x=55, y=180)
+        Ventana.title()
+        etiqueta_total = tk.Label(Ventana, text="COMPARAR FECHAS")
+        etiqueta_total.config(fg="red", bg="#FDF5E6", font=("Open Sans", 20, "bold"))
+        etiqueta_total.place(x=110, y=30)
 
-    etiqueta_cantidad = tk.Label(Ventana, text="GASTOS FECHAS $$$: ")
-    etiqueta_cantidad.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
-    etiqueta_cantidad.place(x=55, y=240)
+        def seleccionar_fecha_a():
+            fecha_a = simpledialog.askstring("Fecha A", "Introduce la fecha (YYYY-MM-DD):")
+            if fecha_a:
+                gastos_a = obtener_gastos(fecha_a)
+                cierre_a = obtener_cierre(fecha_a)
+                if gastos_a == 0 and cierre_a == 0:
+                    messagebox.showwarning("No hay datos", "No se encontro ningun resultado para la consulta")
+                    seleccionar_fecha_a()
+                else:
+                   etiqueta_gastos_a.config(text=f"GASTOS FECHA A: ${gastos_a}")
+                   etiqueta_cierre_a.config(text=f"CIERRE FECHA A: ${cierre_a}")
+                   etiqueta_fecha_a.config(text=f"Fecha A Consultada: {fecha_a}")
 
-    etiqueta_bebidas = tk.Label(Ventana, text="CIERRE$$$: ")
-    etiqueta_bebidas.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
-    etiqueta_bebidas.place(x=55, y=300)
+        def seleccionar_fecha_b():
+            fecha_b = simpledialog.askstring("Fecha B", "Introduce la fecha (YYYY-MM-DD):")
+            if fecha_b:
+                gastos_b = obtener_gastos(fecha_b)
+                cierre_b = obtener_cierre(fecha_b)
+                if gastos_b == 0 and cierre_b == 0:
+                    messagebox.showerror("No hay datos disponibles", "No se encontro ningun resultado para la consulta")
 
-    etiqueta_total_pizas = tk.Label(Ventana, text="FECHA DD/MM/AA: ")
-    etiqueta_total_pizas.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
-    etiqueta_total_pizas.place(x=320, y=180)
+                else:
+                    etiqueta_gastos_b.config(text=f"GASTOS FECHA B: ${gastos_b}")
+                    etiqueta_cierre_b.config(text=f"CIERRE FECHA B: ${cierre_b}")
+                    etiqueta_fecha_b.config(text=f"Fecha B consulta: {fecha_b}")
 
-    etiqueta_total_pizas = tk.Label(Ventana, text="GASTOS FECHAS $$$: ")
-    etiqueta_total_pizas.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
-    etiqueta_total_pizas.place(x=320, y=240)
 
-    etiqueta_total_bebidas = tk.Label(Ventana, text="CIERRE$$$: ")
-    etiqueta_total_bebidas.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
-    etiqueta_total_bebidas.place(x=320, y=300)
+        boton_fecha_a = tk.Button(Ventana, text="FECHA A", command=seleccionar_fecha_a)
+        boton_fecha_a.configure(fg="black", bg="#a6a6a6", font=("Open Sans", 10), width=13)
+        boton_fecha_a.place(x=50,y=100)
 
-    boton_guardar_cierre = tk.Button(Ventana, text="IMPRIMIR REPORTE")
-    boton_guardar_cierre.configure(fg="black", bg="#a6a6a6", font=("Open Sans", 10), width=15)
-    boton_guardar_cierre.place(x=190, y=370)
+        boton_fecha_b = tk.Button(Ventana, text="FECHA B", command=seleccionar_fecha_b)
+        boton_fecha_b.configure(fg="black", bg="#a6a6a6", font=("Open Sans", 10), width=13)
+        boton_fecha_b.place(x=320, y=100)
 
-    volver = tk.Button(Ventana, text="VOLVER", command=volver)
-    volver.configure(fg="black", bg="#a6a6a6", font=("Open Sans", 10), width=15)
-    volver.place(x=190, y=410)
+        etiqueta_total_pizas = tk.Label(Ventana, text="GASTOS FECHA A: ")
+        etiqueta_total_pizas.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_total_pizas.place(x=55, y=180)
 
-    Ventana.iconbitmap(r"C:\Users\Diego Zamora\OneDrive\Documentos\Adsi 2024\interface grafica\recursos\logoico.ico")
+        etiqueta_total_bebidas = tk.Label(Ventana, text="CIERRE FECHA A: ")
+        etiqueta_total_bebidas.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_total_bebidas.place(x=55, y=240)
 
-    Ventana.mainloop()
+        etiqueta_gastos_a = tk.Label(Ventana, text="GASTOS FECHA A: ")
+        etiqueta_gastos_a.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_gastos_a.place(x=55, y=180)
+
+        etiqueta_cierre_a = tk.Label(Ventana, text="CIERRE FECHA A: ")
+        etiqueta_cierre_a.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_cierre_a.place(x=55, y=240)
+
+        etiqueta_gastos_b = tk.Label(Ventana, text="GASTOS FECHA B: ")
+        etiqueta_gastos_b.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_gastos_b.place(x=320, y=180)
+
+        etiqueta_cierre_b = tk.Label(Ventana, text="CIERRE FECHA B: ")
+        etiqueta_cierre_b.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_cierre_b.place(x=320, y=240)
+
+        etiqueta_fecha_a = tk.Label(Ventana,text="fecha A Consultar")
+        etiqueta_fecha_a.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_fecha_a.place(x=55, y=300)
+
+        etiqueta_fecha_b = tk.Label(Ventana, text="Fecha B Consultada: ")
+        etiqueta_fecha_b.configure(fg="black", bg="#FDF5E6", font=("Open Sans", 10))
+        etiqueta_fecha_b.place(x=320, y=300)
+
+
+        volver_btn = tk.Button(Ventana, text="VOLVER", command=volver)
+        volver_btn.configure(fg="black", bg="#a6a6a6", font=("Open Sans", 10), width=15)
+        volver_btn.place(x=190, y=410)
+
+        Ventana.iconbitmap( r"C:\Users\Diego Zamora\OneDrive\Documentos\Adsi 2024\repositorio\Tomas-pizza\recursos\logoico.ico")
+        Ventana.mainloop()
